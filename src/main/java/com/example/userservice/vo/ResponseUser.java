336cd3666148ad653c2057b684440ec0bf2ac6a7
이석준 <seoklee@spectra.co.kr>
2022-09-23 19:00:00 +0900

    getUserInfo

--- src/main/java/com/example/userservice/vo/ResponseUser.java
+++ src/main/java/com/example/userservice/vo/ResponseUser.java
@@ -11,5 +11,5 @@ public class ResponseUser {
     private String email;
     private String name;
     private String userId;
-    private List<ResponseOrder> orders;
+//    private List<ResponseOrder> orders;
 }
